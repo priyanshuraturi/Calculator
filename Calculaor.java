44543088d02a77aacb5a975831d4c99ee65ce0fd
unknown <Priyanshuraturi@gmail.com>
2020-02-11 01:00:31 +0530

    Showing Entered Text Simultaneously

--- Calculaor.java
+++ Calculaor.java
@@ -5,26 +5,39 @@
  */
 package javaapplication3;
 
-import java.util.Scanner;
-
 /**
  *
  * @author Priyanshu Raturi
  */
 public class Calculaor {
-static float op1,op2;//Opreators
-void input(){
-Scanner kb = new Scanner(System.in);
-
-
-}
+static float op1,op2,res;//Opreators
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         Calci cl = new Calci();
         cl.setVisible(true);
+       
         
+        if(Calci.ent){
+            op1=Float.parseFloat(Calci.opreation1);
+            op2=Float.parseFloat(Calci.opreation2);
+            switch(Calci.opreant){
+                case '+':
+                    res=op1=op2;
+                case '-':
+                    res=op1-op2;
+                case 'x':
+                    res=op1*op2;
+                case '/':
+                    res=op1/op2;
+                default:
+                    res=-1;
+                    
+            }
+            System.out.println(res);
+            
+        }
     }
     
 }
